b66ab9becede49c15828b5ec5748e6e392887d42
MohonSaha <practicewithmohon@gmail.com>
2022-06-22 22:42:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Menubar from './Components/Menubar/Menubar';
-import Test from './Components/Test/Test';
-import AllProducts from './Components/AllProducts/AllProducts';
-import { useEffect, useState } from 'react'
-import AOS from "aos";
-import "aos/dist/aos.css";
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const setCartCount = () => {
-    setCount(count + 1);
-  }
-  
-  useEffect(()=>{
-    AOS.init();
-  },[]);
   return (
-    <div className="App">
-      <Menubar count={count}></Menubar>
-      <AllProducts setCartCount={setCartCount}></AllProducts>
+    <div>
+      <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
